b966a8a8ed6ddc8de9a1a58ea71686b218e6eabe
lop3es <lopes.workstation@gmail.com>
2021-11-07 13:33:39 -0300

    feat: formik

--- mobile/src/components/Resgister/index.tsx
+++ mobile/src/components/Resgister/index.tsx
@@ -3,6 +3,8 @@ import { Text } from 'react-native';
 import { View, MainContent, MainLogo, Button, Link } from './style';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as NewIcon from 'react-native-vector-icons/Feather';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import StyledInputBox from '../StyledInputBox'
 
@@ -10,7 +12,28 @@ interface ITest {
     navigation: any
 }
 
+const postForm = (data: any) => {
+    console.log(data)
+}
+
 const Register: React.FC<ITest> = ({navigation}) => {
+    const { values, handleSubmit, handleChange, errors, handleBlur } = useFormik({
+        initialValues: {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validationSchema: Yup.object({
+            username: Yup.string().required('Defina um nome de usuário'),
+            email: Yup.string().email('Formato de e-mail inválido').required('Por favor, insira um e-mail'),
+            password: Yup.string().min(6, 'Senha precisa ter ao menos 6 caracteres'),
+            confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Senhas não batem')
+        }),
+        onSubmit: postForm
+    })
+
+    console.log(errors)
 
     return (
         <View>
@@ -18,7 +41,10 @@ const Register: React.FC<ITest> = ({navigation}) => {
                 <MainLogo> Register </MainLogo>
                 <StyledInputBox
                     width = "95%"
-                    inputPlaceholder = "E-mail"
+                    inputPlaceholder = "Username"
+                    onChange={handleChange('username')}
+                    value={values.username}
+                    onBlur = {handleBlur('username')}
                 >
                     <NewIcon.default
                         name = "user"
@@ -29,6 +55,8 @@ const Register: React.FC<ITest> = ({navigation}) => {
                 <StyledInputBox
                     width = "95%"
                     inputPlaceholder = "E-mail"
+                    onChange={handleChange('email')}
+                    value={values.email}
                 >
                     <Icon 
 
@@ -40,6 +68,9 @@ const Register: React.FC<ITest> = ({navigation}) => {
                 <StyledInputBox
                     width = "95%"
                     inputPlaceholder = "Senha"
+                    onChange={handleChange('password')}
+                    value={values.password}
+                    secureTextEntry = {true}
                 >
                     <Icon 
                         name = "key-variant"
@@ -50,6 +81,9 @@ const Register: React.FC<ITest> = ({navigation}) => {
                 <StyledInputBox
                     width = "95%"
                     inputPlaceholder = "Confirme a senha"
+                    onChange={handleChange('confirmPassword')}
+                    value={values.confirmPassword}
+                    secureTextEntry = {true}
                 >
                     <Icon 
                         name = "key-variant"
@@ -57,7 +91,7 @@ const Register: React.FC<ITest> = ({navigation}) => {
                         color = "#fff"
                     />
                 </StyledInputBox>
-                <Button>
+                <Button onPress = {handleSubmit}>
                     <Text style = {{color: "#fff", fontSize: 18}}> Registrar </Text>
                 </Button>
                 <Link onPress = {() => navigation.navigate('Login')}>
--- mobile/src/components/StyledInputBox/index.tsx
+++ mobile/src/components/StyledInputBox/index.tsx
@@ -4,17 +4,26 @@ import { InputBox, Input, IconBox } from "./style"
 interface IStyledIputBox{
     width: string
     inputPlaceholder: string
+    onChange: (value: any) => any
+    onBlur?: (value: any) => any
+    value: string;
+    secureTextEntry?: boolean
 }
 
-const StyledInputBox: React.FC<IStyledIputBox> = ({width, inputPlaceholder,  children}) => {
+const StyledInputBox: React.FC<IStyledIputBox> = ({width, inputPlaceholder, onChange, onBlur, value, children, secureTextEntry = false}) => {
     return (
         <InputBox width = {width} >
             <IconBox>
                 {children}
             </IconBox>
             <Input
+
                 placeholderTextColor = "#fff"
                 placeholder = {inputPlaceholder}
+                onChangeText={onChange}
+                value={value}
+                onBlur = {onBlur}
+                secureTextEntry = {secureTextEntry}
             />
         </InputBox>
     )
