c63cdacc4cbcd0df61e50ecbebb9a533e18e2fbf
Lopes <lopes.workstation@gmail.com>
2022-01-09 17:03:32 -0300

    fix: authentication middleware

--- backend/src/middlewares/tokenAuthenticate.ts
+++ backend/src/middlewares/tokenAuthenticate.ts
@@ -1,24 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 /**
  * Verifica se um token de autênticação foi informado e se o token é válido.
  * @returns Retorna os dados do usuário parseado em req.body.user.
  */
 
-export const tokenAuthenticate = () => (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization
-    const token = authHeader?.split(" ")[1]
+export const tokenAuthenticate =
+    () => (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization;
 
-    if (!token){
-        return res.status(401).send({message: "Autênticação não fornecida"}).end()
-    }
-
-    jwt.verify(token, String(process.env.HASH_TOKEN_DATA), (err, user) => {
-        if (err) {
-            return res.status(403).send({message: "Token inválido"}).end()
+        if (!token) {
+            return res
+                .status(401)
+                .send({ message: "Autênticação não fornecida" })
+                .end();
         }
-        req.body.user = user
-        next()
-    })
-}
+
+        jwt.verify(token, String(process.env.HASH_TOKEN_DATA), (err, user) => {
+            if (err) {
+                return res
+                    .status(403)
+                    .send({ message: "Token inválido" })
+                    .end();
+            }
+            req.body.user = user;
+            next();
+        });
+    };
